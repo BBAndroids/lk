c59577ea9559e77b9e2b4715f2af814072536b3c
Gor Mirzoyan <personal@xwtk.cloud>
2025-11-02 21:16:58 +0400

    Updated default brightness, implemented Google VID/PID for automatic driver assignment

--- app/aboot/fastboot.c
+++ app/aboot/fastboot.c
@@ -68,11 +68,11 @@ static void fastboot_notify(struct udc_gadget *gadget, unsigned event);
 static struct udc_endpoint *fastboot_endpoints[2];
 
 static struct udc_device surf_udc_device = {
-	.vendor_id    = 0x0FCA,
-	.product_id   = 0x8030,
+	.vendor_id    = 0x18D1,
+	.product_id   = 0x4EE7,
 	.version_id   = 0x0242,
-	.manufacturer = "BlackBerry",
-	.product      = "Passport",
+	.manufacturer = "xwtk.Solutions",
+	.product      = "Harpocrat Bootloader",
 };
 
 static struct udc_gadget fastboot_gadget = {
--- target/msm8974/target_display.c
+++ target/msm8974/target_display.c
@@ -91,9 +91,9 @@ static struct ldo_entry ldo_entry_array_mockingbird[] = {
 
 static struct pm8x41_wled_data wled_ctrl = {
 	.mod_scheme      = 0x00,
-	.led1_brightness = 0x0300,
-	.led2_brightness = 0x0300,
-	.led3_brightness = 0x0300,
+	.led1_brightness = 0x0191,
+	.led2_brightness = 0x0191,
+	.led3_brightness = 0x0191,
 	.max_duty_cycle  = 0x01,
 	.ovp = 0x2,
 	.full_current_scale = 0x19
